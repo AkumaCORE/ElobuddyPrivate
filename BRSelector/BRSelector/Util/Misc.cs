90d225597cc15d624a25181c1fb3f73c1d6f019e
Daniel Paladino <gabrielmlyra@gmail.com>
2015-10-19 22:41:32 -0200

    Sync

---
+++ BRSelector/BRSelector/Util/Misc.cs
@@ -0,0 +1,18 @@
+﻿using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace BRSelector.Util
+{
+    static class Misc
+    {
+        public static bool IsChecked(Menu obj, string value)
+        {
+            return obj[value].Cast<CheckBox>().CurrentValue;
+        }
+
+        public static int GetSliderValue(Menu obj, string value)
+        {
+            return obj[value].Cast<Slider>().CurrentValue;
+        }
+    }
+}
