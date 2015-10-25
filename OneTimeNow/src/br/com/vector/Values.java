78b3cfe67a3d507a64854749827c7d6b6d353e03
Daniel Paladino <gabrielmlyra@gmail.com>
2015-10-25 17:19:36 -0200

    All Champions

--- OneTimeNow/src/br/com/vector/Values.java
+++ OneTimeNow/src/br/com/vector/Values.java
@@ -7,7 +7,7 @@ public final class Values {
 
     private static String ipAdrres;
 
-    private static String dummyData = "{\"PlayersDamage\":[{\"ChampionName\":\"Caitlyn\",\"GoldEarned\":3550.00342,\"TotalDamageDealt\":0,\"PhysicalDamageDealt\":0,\"MagicallDamageDealt\":0},{\"ChampionName\":\"Nasus\",\"GoldEarned\":0,\"TotalDamageDealt\":0,\"PhysicalDamageDealt\":0,\"MagicallDamageDealt\":0},{\"ChampionName\":\"Ryze\",\"GoldEarned\":0,\"TotalDamageDealt\":0,\"PhysicalDamageDealt\":0,\"MagicallDamageDealt\":0},{\"ChampionName\":\"Malphite\",\"GoldEarned\":0,\"TotalDamageDealt\":0,\"PhysicalDamageDealt\":0,\"MagicallDamageDealt\":0},{\"ChampionName\":\"Malzahar\",\"GoldEarned\":0,\"TotalDamageDealt\":0,\"PhysicalDamageDealt\":0,\"MagicallDamageDealt\":0},{\"ChampionName\":\"Zilean\",\"GoldEarned\":0,\"TotalDamageDealt\":0,\"PhysicalDamageDealt\":0,\"MagicallDamageDealt\":0},{\"ChampionName\":\"Morgana\",\"GoldEarned\":0,\"TotalDamageDealt\":0,\"PhysicalDamageDealt\":0,\"MagicallDamageDealt\":0},{\"ChampionName\":\"KogMaw\",\"GoldEarned\":0,\"TotalDamageDealt\":0,\"PhysicalDamageDealt\":0,\"MagicallDamageDealt\":0},{\"ChampionName\":\"MissFortune\",\"GoldEarned\":0,\"TotalDamageDealt\":0,\"PhysicalDamageDealt\":0,\"MagicallDamageDealt\":0},{\"ChampionName\":\"Vladimir\",\"GoldEarned\":0,\"TotalDamageDealt\":0,\"PhysicalDamageDealt\":0,\"MagicallDamageDealt\":0}],\"EnemyTeamGold\":0,\"AllyTeamGold\":3550.00342}";
+    private static String dummyData = "";
 
     public static String getDummyData() {
         return dummyData;
