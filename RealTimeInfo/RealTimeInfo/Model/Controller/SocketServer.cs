1f5e2b9b1f78ccb7255d0b764014606effa87168
Daniel Paladino <gabrielmlyra@gmail.com>
2015-10-25 16:37:11 -0200

    Nice iperino dur

--- RealTimeInfo/RealTimeInfo/Model/Controller/SocketServer.cs
+++ RealTimeInfo/RealTimeInfo/Model/Controller/SocketServer.cs
@@ -38,7 +38,7 @@ namespace RealTimeInfo.Model.Controller
             // Establish the local endpoint for the socket.
             // The DNS name of the computer
             // running the listener is "host.contoso.com".
-            IPHostEntry ipHostInfo = Dns.GetHostEntry("192.168.25.30");
+            IPHostEntry ipHostInfo = Dns.GetHostEntry("127.0.0.1");
             IPAddress ipAddress = ipHostInfo.AddressList[0];
             IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 8080);
 
